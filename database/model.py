16eb86eba00d5213d4028e2763546b724d2cae27
Michael White <mihaliswhite@gmail.com>
2023-11-20 23:30:31 +0000

    Update model.py

--- database/model.py
+++ database/model.py
@@ -6,9 +6,23 @@ user=os.getenv("DATABASE_USER")
 password=os.getenv("DATABASE_PASSWORD"), 
 host=os.getenv("DATABASE_HOST"))
 
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-class Presents(Model):
+class Present(BaseModel):
     name = CharField()
     sku = CharField()
     site = CharField()
+
+class User(BaseModel):
+    username = CharField(unique=True)
+    email = CharField(unique=True) 
+
+class PresentRequests(BaseModel):
+    id = UUIDField(unique=True)
+    user = ForeignKeyField(User, backref='id')
+    present = ForeignKeyField(Present, backref='id')
+    request_timestamp = TimestampField()
+    
 
