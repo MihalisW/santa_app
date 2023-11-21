dac357491939ca7ce22ee3bb6a4c52d86284147c
Michael White <mihaliswhite@gmail.com>
2023-11-21 21:56:35 +0000

    Implement check_health

--- database/queries.py
+++ database/queries.py
@@ -8,11 +8,13 @@ def open_close_connection(func):
     return wrapper
 
 @open_close_connection
-def create_all_tables() -> None: 
+def create_all_tables() -> None:
     db.create_tables([Present, User, PresentRequest])
 
 class Queries(object):
     def __init__(self) -> None:
-        pass
-
-
+        create_all_tables()
+    
+    def check_health(self) -> bool: 
+        cursor = db.execute_sql("SELECT TRUE AS \"value\"").fetchone()[0]
+        return cursor
--- http_layer/server.py
+++ http_layer/server.py
@@ -1,21 +1,44 @@
 import os 
 
-from flask import Flask, render_template, request, Response
+from flask import (Flask, 
+                   render_template, 
+                   request, 
+                   make_response, 
+                   Response)
+import json
 from services.present_service import *
+from database.queries import *
 
 template_dir = os.path.abspath('../santa_app/templates')
 
 def instantiate_app():
     app = Flask(__name__, template_folder=template_dir)
-    
+    queries = Queries()
+
+    def create_response(data, code) -> Response:
+        response = make_response('Response')
+        response.data = data 
+        response.status_code = code    
+        return response
+
     @app.route('/')
     def home_page_route():
         return render_template('index.html')
     
     @app.route('/health', methods = ['GET'])
     def health_route() -> Response:
-        #TODO actually check the database's health
-        return "Ho ho ho", 200
+        response_data = {"app_working": False}
+        try:
+            result = queries.check_health()
+            print(result)
+            response_data["app_working"] = result
+            data = json.dumps(response_data)
+            if result:
+                return create_response(data, 200)
+            return create_response(data, 500)            
+        except Exception as e:
+            return create_response("{\"app_working\":, false}", 200)
+
 
     @app.route('/best-behaviour', methods = ['POST']) 
     def morality_check_route() -> Response:
